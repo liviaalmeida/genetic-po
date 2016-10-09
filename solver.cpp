6812ba4969f67762fc6a3b4ff7faac548f3e98a0
liviaalmeida <leave.ah@gmail.com>
2016-10-08 21:57:36 -0300

    Changes because of use of templates

--- solver.cpp
+++ solver.cpp
@@ -2,8 +2,8 @@
 
 int main(int argc, char const *argv[])
 {
-	GA GenAl(argv[1],argv[2]);
-	Indiv B = GenAl.run();
+	GA<int> GenAl(argv[1],argv[2]);
+	Indiv<int> B = GenAl.run();
 	std::cout<<B;
 	return 0;
 }
